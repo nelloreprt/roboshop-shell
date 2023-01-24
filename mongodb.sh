cc669fcfa3892436744405733dfb2da9be2021d5
nelloreprt <nelloreprt@gmail.com>
2023-01-25 01:04:58 +0530

    create servers script

--- mongodb.sh
+++ mongodb.sh
@@ -14,7 +14,7 @@ systemctl start mongod
 #To check mongodb port number >> 27017  (port range on server 0-65565)
 netstat -lntp | grep mongo
 
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 #Restart the service to make the changes effected.
