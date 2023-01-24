463b056bb8a4fe985bb9798089399b262afb0dbf
nelloreprt <nelloreprt@gmail.com>
2023-01-24 12:48:07 +0530

    shell scripting

--- mongodb.sh
+++ mongodb.sh
@@ -16,3 +16,7 @@ netstat -lntp | grep mongo
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+#Restart the service to make the changes effected.
+systemctl restart mongod
+
