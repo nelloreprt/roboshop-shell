31cbc074d374e9d7a1613d905c015c47384c476b
nelloreprt <nelloreprt@gmail.com>
2023-01-24 14:42:48 +0530

    catalogue shell scripting

--- frontend.sh
+++ frontend.sh
@@ -10,17 +10,18 @@ systemctl enable nginx
 echo -e "\e[31m Start Nginx service\e[0m"
 systemctl start nginx
 
-# Remove the default content that web server is serving.
+echo -e "\e[31m Remove the default content that web server is serving\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-# Download the frontend content
+echo -e "\e[31m Download the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-# Extract the frontend content.
+echo -e "\e[31m Extract the frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 cp -r ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[31m # Restart Nginx Service to load the changes of the configuration\e[0m"
 # Restart Nginx Service to load the changes of the configuration.
 systemctl restart nginx
