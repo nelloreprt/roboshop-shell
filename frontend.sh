498e222ad70887cc5f4ed0c57376ad521ac67dec
nelloreprt <nelloreprt@gmail.com>
2023-01-25 23:23:00 +0530

    create servers script

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,25 @@
 # using camel case, declaring variable for location
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 
 echo -e "\e[31m Install Nginx\e[0m"
-yum install nginx -y
-
-echo -e "\e[31m Enable Nginx service\e[0m"
-systemctl enable nginx
-
-echo -e "\e[31m Start Nginx service\e[0m"
-systemctl start nginx
+yum install nginx -y &>>${LOG}
 
 echo -e "\e[31m Remove the default content that web server is serving\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${LOG}
 
 echo -e "\e[31m Download the frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 
 echo -e "\e[31m Extract the frontend content\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>${LOG}
+unzip /tmp/frontend.zip &>>${LOG}
 
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+
+echo -e "\e[31m Enable Nginx service\e[0m"
+systemctl enable nginx &>>${LOG}
 
 echo -e "\e[31m # Restart Nginx Service to load the changes of the configuration\e[0m"
 # Restart Nginx Service to load the changes of the configuration.
-systemctl restart nginx
+systemctl restart nginx &>>${LOG}
