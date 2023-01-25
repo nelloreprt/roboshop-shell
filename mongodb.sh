f24bec5df5fcd2af63234c12ce79922826712aee
nelloreprt <nelloreprt@gmail.com>
2023-01-26 00:08:32 +0530

    create servers script

--- mongodb.sh
+++ mongodb.sh
@@ -6,16 +6,14 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 #Install MongoDB
 yum install mongodb-org -y
 
-#Start & Enable MongoDB Service
-systemctl enable mongod
-systemctl start mongod
-
-
 #To check mongodb port number >> 27017  (port range on server 0-65565)
-netstat -lntp | grep mongo
+#netstat -lntp | grep mongo
 
 ##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+#Start & Enable MongoDB Service
+systemctl enable mongod
 
 #Restart the service to make the changes effected.
 systemctl restart mongod
