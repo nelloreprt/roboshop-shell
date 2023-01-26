9757876bb9d21f3a1a54345c23bb9be90fc82382
nelloreprt <nelloreprt@gmail.com>
2023-01-26 13:55:58 +0530

    create servers script

--- frontend.sh
+++ frontend.sh
@@ -4,22 +4,31 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[31m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
+echo $?
 
 echo -e "\e[31m Remove the default content that web server is serving\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+echo $?
 
 echo -e "\e[31m Download the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+echo $?
 
 echo -e "\e[31m Extract the frontend content\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
+echo $?
+
 unzip /tmp/frontend.zip &>>${LOG}
+echo $?
 
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
 
 echo -e "\e[31m Enable Nginx service\e[0m"
 systemctl enable nginx &>>${LOG}
+echo $?
 
 echo -e "\e[31m # Restart Nginx Service to load the changes of the configuration\e[0m"
 # Restart Nginx Service to load the changes of the configuration.
 systemctl restart nginx &>>${LOG}
+echo $?
