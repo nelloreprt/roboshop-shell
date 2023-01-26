e127961fa806ba4c72348c970f7de47a5b7f9649
nelloreprt <nelloreprt@gmail.com>
2023-01-26 14:40:10 +0530

    create servers script

--- frontend.sh
+++ frontend.sh
@@ -4,31 +4,79 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[31m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 echo -e "\e[31m Remove the default content that web server is serving\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 echo -e "\e[31m Download the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 echo -e "\e[31m Extract the frontend content\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 echo -e "\e[31m Enable Nginx service\e[0m"
 systemctl enable nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
 
 echo -e "\e[31m # Restart Nginx Service to load the changes of the configuration\e[0m"
 # Restart Nginx Service to load the changes of the configuration.
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo Success
+else
+  echo Fail
+fi
+
