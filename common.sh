6db8dc87e15fd1a34d9c8ff1e1b9524a328b7bdf
nelloreprt <nelloreprt@gmail.com>
2023-01-27 21:40:24 +0530

    create servers script

--- common.sh
+++ common.sh
@@ -16,3 +16,69 @@ print_head () {
   echo " $1 "
 
 }
+
+NODEJS () {
+  print_head "Setup NodeJS repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "Install NodeJS"
+  yum install nodejs -y  &>>${LOG}
+  status_check
+
+  print_head "Add Application User"
+  useradd roboshop &>>${LOG}
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  mkdir -p /app &>>${LOG}
+
+  print_head "Downloading App content"
+  curl -L -o /tmp/{component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip &>>${LOG}
+  status_check
+
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  print_head "Extracting App Content"
+  cd /app
+  unzip /tmp/{component}.zip &>>${LOG}
+  status_check
+
+  print_head "Installing NodeJS Dependencies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
+
+  print_head "Configuring Catalogue Service File"
+  cp ${script_location}/files/{component}.service /etc/systemd/system/{component}.service &>>${LOG}
+  status_check
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable Catalogue Service "
+  systemctl enable {component} &>>${LOG}
+  status_check
+
+  print_head "Start Catalogue service "
+  systemctl start {component} &>>${LOG}
+  status_check
+
+  print_head "Configuring Mongo Repo "
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+  status_check
+
+  print_head "Install Mongo Client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "Load Schema"
+  mongo --host mongodb-dev.nellore.online </app/schema/{component}.js &>>${LOG}
+  status_check
+}
--- user.sh
+++ user.sh
@@ -1,65 +1,7 @@
 source common.sh
 
-print_head "Setup NodeJS repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-status_check
+# declaring variable for the component user
+component=user
 
-print_head "Install NodeJS"
-yum install nodejs -y  &>>${LOG}
-status_check
-
-print_head "Add Application User"
-useradd roboshop &>>${LOG}
-id roboshop &>>${LOG}
-if [ $? -ne 0 ]; then
-  useradd roboshop &>>${LOG}
-fi
-status_check
-
-mkdir -p /app &>>${LOG}
-
-print_head "Downloading App content"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${LOG}
-status_check
-
-print_head "Cleanup Old Content"
-rm -rf /app/* &>>${LOG}
-status_check
-
-print_head "Extracting App Content"
-cd /app
-unzip /tmp/user.zip &>>${LOG}
-status_check
-
-print_head "Installing NodeJS Dependencies"
-cd /app &>>${LOG}
-npm install &>>${LOG}
-status_check
-
-print_head "Configuring Catalogue Service File"
-cp ${script_location}/files/user.service /etc/systemd/system/user.service &>>${LOG}
-status_check
-
-print_head "Reload SystemD"
-systemctl daemon-reload &>>${LOG}
-status_check
-
-print_head "Enable Catalogue Service "
-systemctl enable user &>>${LOG}
-status_check
-
-print_head "Start Catalogue service "
-systemctl start user &>>${LOG}
-status_check
-
-print_head "Configuring Mongo Repo "
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
-status_check
-
-print_head "Install Mongo Client"
-yum install mongodb-org-shell -y &>>${LOG}
-status_check
-
-print_head "Load Schema"
-mongo --host mongodb-dev.nellore.online </app/schema/user.js &>>${LOG}
-status_check
+# to keep the code DRY we sholud remove repetion, so the solution is using "Functions"
+NODEJS
